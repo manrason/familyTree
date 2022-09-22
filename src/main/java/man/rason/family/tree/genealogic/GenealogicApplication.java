f2270d33a1bb155eab85e8e4db48a744d93bf868
titibeloha <manrason2@gmail.com>
2022-09-23 01:07:02 +0200

    Adding init files spring project

---
+++ src/main/java/man/rason/family/tree/genealogic/GenealogicApplication.java
@@ -0,0 +1,13 @@
+package man.rason.family.tree.genealogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenealogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/man/rason/family/tree/genealogic/GenealogicApplicationTests.java
@@ -0,0 +1,13 @@
+package man.rason.family.tree.genealogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenealogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
